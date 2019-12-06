cb2692aaf8c3d247d0b9b2cdf9e64082acccf2db
Hamed <hamedrazor54@gmail.com>
2019-12-06 19:17:22 +0330

    Migrating to AndroidX

--- app/src/main/java/ir/hamedt/floatingvideoplayer/MainActivity.java
+++ app/src/main/java/ir/hamedt/floatingvideoplayer/MainActivity.java
@@ -1,7 +1,7 @@
 package ir.hamedt.floatingvideoplayer;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
